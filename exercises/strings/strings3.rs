a46198554d6090f624a35f3aecd2cab21c04e941
xianxw <391282207@qq.com>
2026-03-13 12:43:32 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,28 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let output = input.trim();
+    output.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let output = input.to_string();
+    let add:&str = " world!";
+    let result:String = output + add;
+    result
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let find = "cars";
+    let output = input.to_string();
+
+    output.replace(find, "balloons")
 }
 
 #[cfg(test)]
